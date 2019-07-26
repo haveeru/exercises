b64d2b46bb6242d981976ee4c98f1a88cc23fcec
haveeru <suood_mohamed@hotmail.com>
2019-07-26 03:35:08 -0700

    Intro Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component, Fragment } from 'react'
-import Header from '../components/Header'
+import {Header, Footer} from './Layouts'
+import Exercises from './excercises'
 
 export default class App extends Component {
   render() {
     return <Fragment>
-          <Header/>
-          <h1>Hi from React</h1>
-        
-      </Fragment>
+            <Header/>
+            <Exercises/>
+            <Footer/>
+          </Fragment>
   }
 }
 
---
+++ src/components/excercises/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Excercises = () => {
+    return (
+        <div>
+            <h1>Hi From Suood</h1>
+        </div>
+    );
+};
+
+export default Excercises;
