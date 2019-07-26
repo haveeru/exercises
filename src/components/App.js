717f8dce56d392bccb104b6ced22d082359e8c26
haveeru <suood_mohamed@hotmail.com>
2019-07-25 22:26:06 -0700

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+import Header from '../components/Header'
+
+export default class App extends Component {
+  render() {
+    return <Fragment>
+          <Header/>
+          <h1>Hi from React</h1>
+        
+      </Fragment>
+  }
+}
+
